4a80775ec3c5744ce2c8911169566d5ba61cb821
Majercsik73 <riasztotnekem@gmail.com>
2021-01-07 20:36:01 +0100

    Saját kútfő.

--- Gyakorlás_41_46_ora_otthon/Gyakorlás_41_46_ora_otthon.py
+++ Gyakorlás_41_46_ora_otthon/Gyakorlás_41_46_ora_otthon.py
@@ -2,27 +2,27 @@
 def gyakorlas():
     f = open("c:/Users/Dell/Documents/SZOFTVERFEJLESZTŐ/Programozás/adatsorok.txt", 'r')
     diakok = f.read().split('\n')
+    f.close
     print(diakok)
-    f.close()
     print()
 
-    lista = []
+    eredmenyek = []
     for i in range(0,len(diakok)):
         sor = diakok[i].split(' ')
         osszeg = 0
         for j in range(1,len(sor)):
             osszeg += int(sor[j])
-        lista.append(osszeg)
-    print(lista)
+        eredmenyek.append(osszeg)
+    print(eredmenyek)
     print()
 
     maxi = 0
-    for k in range(0,len(lista)):
-        if(lista[k]>lista[maxi]):
+    for k in range(0,len(eredmenyek)):
+        if (eredmenyek[k] > eredmenyek[maxi]):
             maxi = k
     print(maxi+1)
-    sv = diakok[maxi].split(' ')
-    print("A legjobb versenyző: ",sv[0])
+    seged = diakok[maxi].split(' ')
+    print("A legeredményesebb diák: ", seged[0])
     print()
 
 
